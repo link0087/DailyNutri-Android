53c10eb1b8ee4a25f676d0fa6cbea471ce1a2acc
wyn <wyn@Fabio-PC>
2016-03-24 23:45:38 +0100

    Login controller e ngroute per login

--- www/js/index.js
+++ www/js/index.js
@@ -47,3 +47,57 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+//angular.module('DailyNutri', ['ngMaterial']);
+(function () {
+    'use strict';
+ 
+    var app = angular.module('DailyNutri', ['ngMaterial', 'ngRoute'])
+    .config(config);
+ 
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+            	controller: 'LoginController',
+                templateUrl: './v_login/loginForm.html'
+            })
+ 
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: './v_login/loginForm.html'
+            })
+ 
+            .when('/register', {
+                controller: 'RegisterController',
+                templateUrl: 'register/register.view.html'
+            })
+ 
+            .otherwise({ redirectTo: '/login' });
+    }
+    
+    app.controller('LoginController', function($scope) {
+        $scope.firstName = "John";
+        $scope.lastName = "Doe";
+    });
+ 
+//    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+//    function run($rootScope, $location, $cookieStore, $http) {
+//    	$location.path('/login');
+//        // keep user logged in after page refresh
+//        $rootScope.globals = $cookieStore.get('globals') || {};
+//        if ($rootScope.globals.currentUser) {
+//            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+//        }
+// 
+//        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+//            // redirect to login page if not logged in and trying to access a restricted page
+//            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+//            var loggedIn = $rootScope.globals.currentUser;
+//            if (restrictedPage && !loggedIn) {
+//                $location.path('/login');
+//            }
+//        });
+//    }
+ 
+})();
